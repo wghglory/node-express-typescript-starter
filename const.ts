838fd364b1142bbd04b95f03f84404cf3124b819
Guanghui Wang <guanghui-wang@foxmail.com>
2022-08-09 20:21:25 +0800

    feat: update auth middleware

--- const.ts
+++ const.ts
@@ -1,1 +1,2 @@
 export const apiPrefix = `/api/v1`;
+export const X_VCLOUD_AUTHORIZATION = `x-vcloud-authorization`;
--- server.ts
+++ server.ts
@@ -1,20 +1,23 @@
 import cors from 'cors';
 import express, {Application, Request, Response} from 'express';
 
-import {apiPrefix} from './const';
+import authMiddleware from './middlewares/auth-middleware';
 import {router as authRouter} from './routes/auth.route';
 import {router as tenantRouter} from './routes/tenant.route';
 
 const app: Application = express();
 const port = 3000;
+const apiPrefix = '/cloudapi/1.0.0'; // '/api/v1'
 
 // Body parsing Middleware
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
+app.use(authMiddleware);
+
 app.use(`${apiPrefix}/tenants`, tenantRouter);
-app.use(`${apiPrefix}`, authRouter);
+app.use(`/api`, authRouter);
 
 // app.get('/', async (req: Request, res: Response): Promise<Response> => {
 //   return res.status(200).send({
